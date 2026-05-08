1b428dbefa43d1d5dba2a95d4ce0934fb5fa7c01
GreemDev <greemdev@ryujinx.app>
2026-05-07 21:51:22 -0500

    Future URL

--- src/resources/once-ui.config.ts
+++ src/resources/once-ui.config.ts
@@ -13,8 +13,8 @@ import {
 } from "@/types";
 import { home } from "./index";
 
-// IMPORTANT: Replace with your own domain address - it's used for SEO in meta tags and schema
-const baseURL: string = "https://demo.magic-portfolio.com";
+// IMPORTANT: used for SEO in meta tags and schema
+const baseURL: string = "https://greemdev.net";
 
 const routes: RoutesConfig = {
   "/": true,
